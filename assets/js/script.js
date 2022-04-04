da55c3c7908134edc5c2eff86e70846375418ed0
sharkbyte <sgquin@gmail.com>
2022-04-03 22:03:10 -0700

    game loop, showHiScores, return to start page buttons, show/hide appropriate things

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,31 @@
 var startBut = document.getElementById('startBut')
 var startPage = document.getElementById('start')
 var game = document.getElementById('game')
-var hiScores = document.getElementById('hiScores')
+var hiScoresBut = document.getElementById('hiScoresButton')
+var hiScoresPage = document.getElementById('hiScores')
+var back = document.getElementById('back')
  
-startBut.addEventListener("click", function(){
-  gameLoop()
-})
+startBut.addEventListener("click", gameLoop)
+hiScoresBut.addEventListener("click", showHiScores)
+back.addEventListener("click", showStartPage)
+
+
+
 
 
 function gameLoop() {
   startPage.setAttribute("style", "display: none")
+  hiScoresBut.setAttribute("style", "display: none")
 }
 
-
 function showHiScores() {
+  hiScoresPage.setAttribute("style", "display: flex") 
+  startPage.setAttribute("style", "display: none")
+  hiScoresBut.setAttribute("style", "display: none")
+}
 
+function showStartPage() {
+  hiScoresBut.setAttribute("style", "display: flex")
+  startPage.setAttribute("style", "display: flex")
+  hiScoresPage.setAttribute("style", "display: none") 
 }
